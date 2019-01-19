918d6b0c181337cab54f52cd64398883faf1c7c4
AdilaHalik <a.dile813>
2019-01-19 16:40:53 -0500

    changes multiply method

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,7 +13,9 @@ public class Calculator {
     }
 
     public static double multiply ( double num1 , double num2){
-        
-        return num1 * num2 ;
+        double sum= num1 * num2;
+        return  sum;
     }
+
+
 }
